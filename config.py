651151e6e279b7b312f044703763d28fc07928d4
sadtafdhxbvx3322-rgb <sadtafdhxbvx3322@gmail.com>
2025-12-12 01:15:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,15 @@ import os
 
 class Config:
     # =====================================================
-    # 🔐 TAU KA FINAL CONFIGURATION
+    # 🔐 TAU KA FINAL CONFIGURATION (Session Added ✅)
     # =====================================================
 
-    # 1. INSTAGRAM
+    # 1. INSTAGRAM (Login Bypass Mode)
     INSTA_USER = "khooshzzzz"
     INSTA_PASS = "Khush@123262669"
+    
+    # 👇 TERA ASLI BRAHMASTRA (Maine JSON fit kar diya hai)
+    INSTA_SESSION = """{"uuids": {"phone_id": "696e1145-6bb1-4cc7-924d-1de59748161e", "uuid": "5262bd56-cef6-41b1-abfa-3b86c3936282", "client_session_id": "e842ffb3-153a-4779-8d29-dd9cd9d03744", "advertising_id": "a9ae3e13-2bcd-410f-8468-e8b198a0020f", "android_device_id": "android-22655f8427eadc9e", "request_id": "d0d61cb6-fd83-449d-9f86-50d40ad7b11c", "tray_session_id": "c95f775f-e8cb-46c1-8d56-f009dc7529f8"}, "mid": "aTse8QABAAFwBm3sx5EPl6kbERWU", "ig_u_rur": null, "ig_www_claim": null, "authorization_data": {"ds_user_id": "75136570684", "sessionid": "75136570684%3AaSjWEEQeYHMqtn%3A2%3AAYisVwyK5pUum1qI8hLmKAUsvy00ysQqbwl4g6-lVQ"}, "cookies": {}, "last_login": 1765482233.951577, "device_settings": {"app_version": "269.0.0.18.75", "android_version": 26, "android_release": "8.0.0", "dpi": "480dpi", "resolution": "1080x1920", "manufacturer": "OnePlus", "device": "devitron", "model": "6T Dev", "cpu": "qcom", "version_code": "314665256"}, "user_agent": "Instagram 269.0.0.18.75 Android (26/8.0.0; 480dpi; 1080x1920; OnePlus; 6T Dev; devitron; qcom; en_US; 314665256)", "country": "US", "country_code": 1, "locale": "en_US", "timezone_offset": -14400}"""
 
     # 2. AI BRAIN
     GEMINI_KEY = "AIzaSyD22dR3viKxaLyIWeOGJxKmrLckV4Q-NOU"
@@ -18,11 +21,8 @@ class Config:
     # 4. TELEGRAM BRIDGE
     TG_API_ID = 31908861
     TG_API_HASH = "db7b4118965e302e60cf66cc89570166"
-    
-    # 👇 TERA NAYA SESSION STRING (Jo tune abhi bheja)
     SESSION_STRING = "BQHm4_0Axqq3Ep2NJjSipYcZeCllIx5jnZk9rA5rH98NncWoxmaZJFqIJw0Rz59q2FQxjfAfzpSiPpQRTwWecqpZB70t-qmxPwec1FH-lxlDg3FppaxBJPTL7rWEW8Rj6b8SMBw4phAl4wFVwFgz7_T_MGubK4lgLmXj0IZy0x0V7G0E4VEBM9LXr8HsRNbvrKsEjXpGIz-HOq-ZBtuLQ8jA4Ya8WUnXAnoyEK5nsrMLgzu0_AP4cZXxwlejnDu65Aah49obP7Nmr4JvydNk2fDtwK1goGz1R4q4Sl62VSSBeRlP6b1DdbI7y3cgl1wt1580Zt_Jp8L1rwy-UFhwbESojtR6PAAAAAHrIlL5AA"
 
-    # 5. TARGET BOT LIST (Primary & Backup)
-    # Hum pehle CyberInfo try karenge, agar wo fail hua toh TrueCallerRobot
+    # 5. BOTS
     PRIMARY_BOT = "CYBERINFOXXXBOT"
     BACKUP_BOT = "TrueCalleRobot"
